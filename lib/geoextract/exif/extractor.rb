702e229b517703c8dde2ef10ccb2c53ed2428565
Gui Vieira <guialbuk@gmail.com>
2019-07-11 21:55:54 -0700

    Type cast  extracted GPD data

--- lib/geoextract/exif/extractor.rb
+++ lib/geoextract/exif/extractor.rb
@@ -8,12 +8,25 @@ module Geoextract
       end
 
       def extract_gps_data
-        ::Exif::Data.new(file)[:gps]
+        cast_values(gps_data)
+
       rescue ::Exif::NotReadable
         {}
       end
 
       private
+
+      def gps_data
+        ::Exif::Data.new(file)[:gps]
+      end
+
+      def cast_values(gps_data)
+        {}.tap do |data|
+          gps_data.each do |key, value|
+            data[key] = Type::Caster.new(key, value).cast
+          end
+        end
+      end
 
       def file
         IO.read(@absolute_path)
